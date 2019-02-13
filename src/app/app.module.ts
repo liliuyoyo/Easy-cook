67a21fdb30104299d30d7a6b96ba1fd2930ef8f5
liyooo <liliuyoyo@gmail.com>
2019-02-13 14:58:53 -0600

    add header section to the root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+
 
 @NgModule({
   declarations: [
@@ -16,10 +18,10 @@ import { RecipeBookComponent } from './recipe-book/recipe-book.component';
     HeaderComponent,
     ShoppingListComponent,
     RecipeListComponent,
-    RecipeItemComponent,
     RecipeDetailComponent,
     ShoppingListEditComponent,
-    RecipeBookComponent
+    RecipeBookComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule

b611592d285a6166af3aae86d356fbc87512388a
liyooo <liliuyoyo@gmail.com>
2019-02-17 06:00:53 -0600

    dropdown directive added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail
     RecipeDetailComponent,
     ShoppingListEditComponent,
     RecipeBookComponent,
-    RecipeItemComponent
+    RecipeItemComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule
